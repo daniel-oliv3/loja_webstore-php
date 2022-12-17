8358b42f38dcf937cc065e0db39d0888fab6dc1e
Daniel Oliveira <danielsapup3@gmail.com>
2022-12-17 09:44:38 -0300

    Update

--- web_010/core/views/layouts/footer.php
+++ web_010/core/views/layouts/footer.php
@@ -1,3 +1,4 @@
+<!-- ======= FOOTER ======= -->
 <div class="container-fluid fixed-bottom rodape">
     <div class="row">
         <div class="col-12 text-center p-3">
--- web_010/core/views/layouts/header.php
+++ web_010/core/views/layouts/header.php
@@ -1,3 +1,9 @@
+<?php
+use core\classes\Store;
+
+//$_SESSION['cliente'] = 1;
+?>
+<!-- ======= HEADER ======= -->
 <div class="container-fluid navegacao"> 
     <div class="row">
         <div class="col-6 p-3">
@@ -9,7 +15,16 @@
             
             <a href="?a=inicio">Início</a>
             <a href="?a=loja">Loja</a>
-            
+
+            <!-- Verifica se existe cliente na sessão -->
+            <?php if(Store::clienteLogado()): ?>
+                <a href="#">Minha conta</a>
+                <a href="#">Logout</a>
+            <?php else: ?>
+                <a href="#">Login</a>
+                <a href="#">Criar conta</a>
+            <?php endif; ?>
+
             <a href="?a=carrinho"><i class="fas fa-shopping-cart"></i></a>
             <span class="badge bg-warning">10</span>
         </div>
