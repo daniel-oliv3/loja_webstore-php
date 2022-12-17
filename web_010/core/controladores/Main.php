a4ad715645df890ca255b33331a31a9fcf5bed0d
Daniel Oliveira <danielsapup3@gmail.com>
2022-12-16 22:24:55 -0300

    Update

--- web_010/core/controladores/Main.php
+++ web_010/core/controladores/Main.php
@@ -9,23 +9,26 @@ class Main {
     /*==============================================================*/
     public function index(){
 
-        $dados = [
-            'titulo' => APP_NAME . " " . APP_VERSION,
-        ];
-
         Store::Layout([
             'layouts/html_header',
             'layouts/header',
             'inicio',
             'layouts/footer',
             'layouts/html_footer',
-        ], $dados);
+        ]);
     }
 
 
     /*==============================================================*/
     public function loja(){
-        echo "Loja!";
+        //Apresenta a página da loja
+        Store::Layout([
+            'layouts/html_header',
+            'layouts/header',
+            'loja',
+            'layouts/footer',
+            'layouts/html_footer',
+        ]);
     }
 }
 
