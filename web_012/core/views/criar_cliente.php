f924b23d4d957d9c3bb0c25b2da6e7f0e4f369f9
Daniel Oliveira <danielsapup3@gmail.com>
2022-12-18 09:39:59 -0300

    Update criar_cliente.php

--- web_012/core/views/criar_cliente.php
+++ web_012/core/views/criar_cliente.php
@@ -1,7 +1,35 @@
 <div class="container">
     <div class="row my-5">
-        <div class="col-12">
+        <div class="col-sm-6 offset-sm-3">
             <h3 class="text-center">Registro de Novo Cliente</h3>
+
+            <form action="" method="post">                
+                <!-- Nome -->
+                <div class="form-group">
+                    <label>Nome Completo</label>                    
+                    <input type="text" class="form-control" name="text_name" placeholder="Digite Seu Nome Completo" required>
+                </div>
+                <!-- Email -->
+                <div class="form-group">
+                    <label>Email</label>
+                    <input type="email" class="form-control" name="text_email" placeholder="Digite Seu E-mail" required>
+                </div>
+                <!-- Senha -->
+                <div class="form-group">
+                    <label>Senha</label>
+                    <input type="password" class="form-control" name="text_senha_1" placeholder="Digite Sua Senha" required>
+                </div>
+                <!-- Confirmação da Senha -->
+                <div class="form-group">
+                    <label>Confirmar Senha</label>
+                    <input type="password" class="form-control" name="text_senha_2" placeholder="Confirme Sua Senha" required>
+                </div>
+                <!-- Endereço -->
+                <div class="form-group">
+                    <label>Endereço</label>
+                    <input type="text" class="form-control" name="text_endereco" placeholder="Digite Seu Endereço" required>
+                </div>
+            </form>        
         </div>
     </div>
 </div>
