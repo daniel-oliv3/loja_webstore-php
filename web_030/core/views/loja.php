78531227d68ed8bc5c6655f1b0117bb25c0e8b3d
Daniel Oliveira <danielsapup3@gmail.com>
2023-01-04 12:14:36 -0400

    Update loja.php

--- web_030/core/views/loja.php
+++ web_030/core/views/loja.php
@@ -20,6 +20,7 @@
                 <h3>Não existem produtos disponíveis!</h3>
             </div>
         <?php else:?>
+            <!-- Ciclo de apresentação dos produtos -->
             <?php foreach($produtos as $produto): ?>
             <div class="col-sm-4 col-6 p-2">
                 <div class="text-center p-3 box-produto">
@@ -27,7 +28,11 @@
                     <h5><?= $produto->nome_produto ?></h5>
                     <h6><?= 'R$ ' . preg_replace("/\./", ",", $produto->preco)?></h6>
                     <div>
-                        <button class="btn btn-warning btn-sm" onclick="adicionar_carrinho(<?= $produto->id_produto ?>)"><i class="fas fa-shopping-cart me-2"></i>Adicionar ao carrinho</button>
+                        <?php if($produto->estoque <= 0): ?>
+                                <button class="btn btn-secondary btn-sm"><i class="fas fa-shopping-cart me-2"></i>Produto Indisponível</button>
+                            <?php else: ?>
+                                <button class="btn btn-warning btn-sm" onclick="adicionar_carrinho(<?= $produto->id_produto ?>)"><i class="fas fa-shopping-cart me-2"></i>Adicionar ao Carrinho</button>
+                        <?php endif; ?>
                     </div>
                 </div>
             </div>
