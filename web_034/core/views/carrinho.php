808a4d63e111c575909378e6b12206c4504c8c91
Daniel Oliveira <danielsapup3@gmail.com>
2023-01-11 13:17:05 -0400

    Update carrinho.php

--- web_034/core/views/carrinho.php
+++ web_034/core/views/carrinho.php
@@ -1,60 +1,68 @@
-<div class="container-fluid"></div>
-    <div class="row">
-        <div class="col-12">
-            <h1>Carrinho</h1>
+<h1 class="text-center">Carrinho</h1>
 
-            <div class="container">
-                <div class="row">
-                    <div class="col">
-                        <?php if($carrinho == null): ?>
-                            <p>Carrinho vazio</p>
-                            <p><a href="?a=loja" class="btn btn-primary">Loja</a></p>
-                            <?php else: ?>
-                            <table class="table">
-                                <thead>
+
+<div class="container">
+    <div class="row">
+        <div class="col">
+            
+            <?php if ($carrinho == null) : ?>
+                <p>Carrinho vazio</p>
+                <p><a href="?a=loja" class="btn btn-primary">Loja</a></p>
+                <?php else : ?>
+                    
+                    <div style="margin-bottom: 80px;">
+                        <table class="table">
+                            <thead>
+                            <tr>
+                                <th></th>
+                                <th>Produto</th>
+                                <th>Quantidade</th>
+                                <th class="text-end">Valor total</th>
+                                <th></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php
+                            $index = 0;
+                            $total_rows = count($carrinho);
+                            ?>
+                            <?php foreach ($carrinho as $produto) : ?>
+                                <?php if ($index < $total_rows - 1) : ?>
+                                    
+                                    <!-- lista dos produtos -->
                                     <tr>
-                                        <th></th>
-                                        <th>Produto</th>
-                                        <th>Quantidade</th>
-                                        <th>Valor total</th>
-                                        <th></th>
+                                        <td><img src="assets/img/produtos/<?= $produto['imagem']; ?>" class="img-fluid" width="50px"></td>
+                                        <td><?= $produto['titulo'] ?></td>
+                                        <td><?= $produto['quantidade'] ?></td>
+                                        <td class="text-end"><?= $produto['preco'] ?></td>
+                                        <td><button class="btn btn-danger"><i class="fas fa-times"></i></button></td>
                                     </tr>
-                                </thead>
-                                <tbody>
-                                    <?php
-                                        $index = 0;
-                                        $total_rows = count($carrinho);
-                                    ?>
-                                    <?php foreach($carrinho as $produto): ?>
-                                        <?php if($index < $total_rows-1): ?>
-                                        <!-- Lista dos produtos -->
-                                        <tr>
-                                            <td><img src="assets/img/produtos/<?= $produto['imagem']; ?>" class="img-fluid"></td>
-                                            <td><?= $produto['titulo'] ?></td>
-                                            <td><?= $produto['quantidade'] ?></td>
-                                            <td><?= $produto['preco'] ?></td>
-                                            <td>Ações</td>
-                                        </tr>
-                                        <?php else: ?>
-                                        <!-- Total -->
+                                    
+                                    <?php else : ?>
+                                        
+                                        <!-- total -->
                                         <tr>
                                             <td>Total:</td>
                                             <td></td>
                                             <td></td>
-                                            <td><?= $produto ?></td>
+                                            <td class="text-end"><?= $produto ?></td>
                                             <td></td>
                                         </tr>
+                                        
                                         <?php endif; ?>
                                         <?php $index++; ?>
-                                    <?php endforeach; ?>
-                                </tbody>
-                            </table>
-                        <?php endif; ?>
+                                        <?php endforeach; ?>
+                                    </tbody>
+                                </table>
+                            </div>
+                            <?php endif; ?>
+                        </div>
                     </div>
                 </div>
-            </div>
+                
+                <a href="?a=limpar_carrinho" class="btn btn-warning"><i class="fas fa-shopping-cart"></i> Limpar Carrinho</a>
+                
+                <div class="espaco-fundo">
+                <!-- ESPAÇO (div tmp) -->
+                </div>
 
-            <a href="?a=limpar_carrinho" class="btn btn-warning"><i class="fas fa-shopping-cart"></i> Limpar Carrinho</a>
-        </div>
-    </div>
-</div>
